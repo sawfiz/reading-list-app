28758e5355ce2845e329b40de13aca7fad888d78
sawfiz <dashu.up@gmail.com>
2023-07-09 15:55:51 +0800

    Change tooltip styling

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 
+import styled from 'styled-components';
+
 import { BookDetailsContext } from '../contexts/BookDetailsContext';
 
 import AddBookModal from './AddBookModal';
@@ -11,18 +13,32 @@ import addButtonImg from '../images/icons8-add-96.png';
 export default function AddBook() {
   const { isAddModalOpen, openAddModal } = useContext(BookDetailsContext);
 
+  const S = {};
+
+  S.Button = styled.button`
+    border: none;
+  `;
+
   return (
-    <div>
-      <button
+    <>
+      <S.Button
+        override
         data-tooltip-id="addBookBtn"
         data-tooltip-content="Add a new book"
         data-tooltip-place="right"
         onClick={openAddModal}
       >
         <img className="addBookImg" src={addButtonImg} alt="" />
-      </button>
+      </S.Button>
       {isAddModalOpen && <AddBookModal />}
-      <ReactTooltip id="addBookBtn" />
-    </div>
+      <ReactTooltip
+        id="addBookBtn"
+        style={{
+          backgroundColor: 'var(--color-dark)',
+          color: '#222',
+          fontFamily: 'sans-serif',
+        }}
+      />
+    </>
   );
 }
